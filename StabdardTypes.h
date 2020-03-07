fe4d3853b74186464eb27b5e7d4a33c997e88c1d
Mostafa Ashraf <mostaphaashraf1996@gmail.com>
2020-03-07 02:42:08 +0200

    C99 Standard data types

---
+++ StabdardTypes.h
@@ -0,0 +1,30 @@
+
+/* Documentation Section
+//  This file have all standard Variables Types in C99_Data_Type
+//  Author: Mostafa Ashraf
+//  Date: 26/02/2020
+*/
+#ifndef	STANDERD_TYPES_H 
+#define STANDERD_TYPES_H 
+
+typedef unsigned char											uint_8;
+typedef 				 char											int_8;
+typedef	volatile char 		 								vint_8;
+
+typedef unsigned short										uint_16;
+typedef 				 short										int_16;
+typedef	volatile short 		 								vint_16;
+
+typedef					 long 										int_32;
+typedef unsigned long	 										uint_32;
+typedef	volatile long 		 								vint_32;
+
+typedef					 long long								int_64;
+typedef unsigned long long								uint_64;
+typedef	volatile long long 								vsint64;
+
+typedef float 														float_32;
+typedef double			 											float_64;
+typedef long	double 											float_96;
+
+#endif /*	STANDERD_TYPES_H */
