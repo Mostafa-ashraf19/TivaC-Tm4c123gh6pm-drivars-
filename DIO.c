667c8c7c2e65192f70b0f810ffc56f47d2a602c4
Mostafa Ashraf <mostaphaashraf1996@gmail.com>
2020-03-07 02:46:47 +0200

    general purpose input output first version with main operations

---
+++ DIO.c
@@ -0,0 +1,289 @@
+/*
+//  This file have implemntions for prototype functions in dio.h 
+//  Author: Mostafa Ashraf
+//  Date: 7/03/2020
+*/
+#include "DIO.h"
+/*
+	Function Name        : Dio_config
+	Function Returns     : void
+	Function Arguments   : PortName portname,PinNum pinnumber,Mode mode
+	Function Description : modified port critria depend on some operations and modes for each pin 
+*/
+void Dio_config(PortName portname,PinNum pinnumber,Mode mode)
+{
+	SET_BIT(SYSCTL_RCGC2_R,portname);
+	// access clock for certin port depend on it's name
+	while(IS_BIT_SET(SYSCTL_PRGPIO_R,portname) == 0);
+	// check for PORT_n Ready
+	switch(portname)
+	{
+		case PORTA:
+			GPIO_PORTA_LOCK_R= MAGIC_NUMBER; // for un lock port
+			// should her put commit reg but default value if (FF) maybe putted next time or new virsion  
+			SET_BIT(GPIO_PORTA_DEN_R,pinnumber);// enable digital mode
+			CLEAR_BIT(GPIO_PORTA_AFSEL_R,pinnumber); // disable altenate function
+			// should her put port control reg but default value is (0-) maybe putted next time or new virsion 
+			CLEAR_BIT(GPIO_PORTA_AMSEL_R,pinnumber);//disable analog mode
+			switch(mode)
+			{
+				case INPUT:
+					CLEAR_BIT(GPIO_PORTA_DIR_R,pinnumber);
+					break;
+				case OUTPUT:
+					SET_BIT(GPIO_PORTA_DIR_R,pinnumber);
+					break;
+				
+				case INTERNAL_PULL_UP:
+					break;
+				
+				case INTERNAL_PULL_DOWN:
+					break;
+				
+				default:
+					break;
+			}
+		break;
+			
+		case PORTB:
+			GPIO_PORTB_LOCK_R= MAGIC_NUMBER; // for un lock port
+			// should her put commit reg but default value if (FF) maybe putted next time or new virsion  
+			SET_BIT(GPIO_PORTB_DEN_R,pinnumber);// enable digital mode
+			CLEAR_BIT(GPIO_PORTB_AFSEL_R,pinnumber); // disable altenate function
+			// should her put port control reg but default value is (0-) maybe putted next time or new virsion 
+			CLEAR_BIT(GPIO_PORTB_AMSEL_R,pinnumber);//disable analog mode
+			switch(mode)
+			{
+				case INPUT:
+					CLEAR_BIT(GPIO_PORTB_DIR_R,pinnumber);
+					break;
+				case OUTPUT:
+					SET_BIT(GPIO_PORTB_DIR_R,pinnumber);
+					break;
+				
+				case INTERNAL_PULL_UP:
+					break;
+				
+				case INTERNAL_PULL_DOWN:
+					break;
+				
+				default:
+					break;
+			}
+			break;
+			case PORTC:
+				GPIO_PORTC_LOCK_R= MAGIC_NUMBER; // for un lock port
+			// should her put commit reg but default value if (FF) maybe putted next time or new virsion  
+			SET_BIT(GPIO_PORTC_DEN_R,pinnumber);// enable digital mode
+			CLEAR_BIT(GPIO_PORTC_AFSEL_R,pinnumber); // disable altenate function
+			// should her put port control reg but default value is (0-) maybe putted next time or new virsion 
+			CLEAR_BIT(GPIO_PORTC_AMSEL_R,pinnumber);//disable analog mode
+			switch(mode)
+			{
+				case INPUT:
+					CLEAR_BIT(GPIO_PORTC_DIR_R,pinnumber);
+					break;
+				case OUTPUT:
+					SET_BIT(GPIO_PORTC_DIR_R,pinnumber);
+					break;
+				
+				case INTERNAL_PULL_UP:
+					break;
+				
+				case INTERNAL_PULL_DOWN:
+					break;
+				
+				default:
+					break;
+			}
+			break;		
+			case PORTD:
+				GPIO_PORTD_LOCK_R= MAGIC_NUMBER; // for un lock port
+			// should her put commit reg but default value if (FF) maybe putted next time or new virsion  
+			SET_BIT(GPIO_PORTD_DEN_R,pinnumber);// enable digital mode
+			CLEAR_BIT(GPIO_PORTD_AFSEL_R,pinnumber); // disable altenate function
+			// should her put port control reg but default value is (0-) maybe putted next time or new virsion 
+			CLEAR_BIT(GPIO_PORTD_AMSEL_R,pinnumber);//disable analog mode
+			switch(mode)
+			{
+				case INPUT:
+					CLEAR_BIT(GPIO_PORTD_DIR_R,pinnumber);
+					break;
+				case OUTPUT:
+					SET_BIT(GPIO_PORTD_DIR_R,pinnumber);
+					break;
+				
+				case INTERNAL_PULL_UP:
+					break;
+				
+				case INTERNAL_PULL_DOWN:
+					break;
+				
+				default:
+					break;
+			}
+			break;
+			case PORTE:
+				GPIO_PORTE_LOCK_R= MAGIC_NUMBER; // for un lock port
+			// should her put commit reg but default value if (FF) maybe putted next time or new virsion  
+			SET_BIT(GPIO_PORTE_DEN_R,pinnumber);// enable digital mode
+			CLEAR_BIT(GPIO_PORTE_AFSEL_R,pinnumber); // disable altenate function
+			// should her put port control reg but default value is (0-) maybe putted next time or new virsion 
+			CLEAR_BIT(GPIO_PORTE_AMSEL_R,pinnumber);//disable analog mode
+			switch(mode)
+			{
+				case INPUT:
+					CLEAR_BIT(GPIO_PORTE_DIR_R,pinnumber);
+					break;
+				case OUTPUT:
+					SET_BIT(GPIO_PORTE_DIR_R,pinnumber);
+					break;
+				
+				case INTERNAL_PULL_UP:
+					break;
+				
+				case INTERNAL_PULL_DOWN:
+					break;
+				
+				default:
+					break;
+			}
+			break;
+			case PORTF:
+							GPIO_PORTF_LOCK_R= MAGIC_NUMBER; // for un lock port
+			// should her put commit reg but default value if (FF) maybe putted next time or new virsion  
+			SET_BIT(GPIO_PORTF_DEN_R,pinnumber);// enable digital mode
+			CLEAR_BIT(GPIO_PORTF_AFSEL_R,pinnumber); // disable altenate function
+			// should her put port control reg but default value is (0-) maybe putted next time or new virsion 
+			CLEAR_BIT(GPIO_PORTF_AMSEL_R,pinnumber);//disable analog mode
+			switch(mode)
+			{
+				case INPUT:
+					CLEAR_BIT(GPIO_PORTF_DIR_R,pinnumber);
+					break;
+				case OUTPUT:
+					SET_BIT(GPIO_PORTF_DIR_R,pinnumber);
+					break;
+				
+				case INTERNAL_PULL_UP:
+					break;
+				
+				case INTERNAL_PULL_DOWN:
+					break;
+				
+				default:
+					break;
+			}
+			break;		
+			
+		default:
+			break;
+	}
+}
+/*
+	Function Name        : Dio_Write
+	Function Returns     : void
+	Function Arguments   : PortName portname,PinNum pinnum,Signal type
+	Function Description : Set the value of the given pin in the given port (outputvalue LOW=0,HIGH=1) 
+*/
+void Dio_Write_Pin(PortName portname,PinNum pinnum,Signal type)
+{
+	switch (portname)
+			{
+				case PORTA:
+					if ( type == HIGH )
+					SET_BIT(GPIO_PORTA_DATA_R,pinnum);
+					else 
+					CLEAR_BIT(GPIO_PORTA_DATA_R,pinnum);
+					break;
+					
+				case PORTB:
+					if ( type == HIGH )
+					SET_BIT(GPIO_PORTB_DATA_R,pinnum);
+					else 
+					CLEAR_BIT(GPIO_PORTB_DATA_R,pinnum);
+					break;
+					
+				case PORTC:
+					if ( type == HIGH )
+					SET_BIT(GPIO_PORTC_DATA_R,pinnum);
+					else 
+					CLEAR_BIT(GPIO_PORTC_DATA_R,pinnum);
+					break;
+					
+				case PORTD:
+					if ( type == HIGH )
+					SET_BIT(GPIO_PORTD_DATA_R,pinnum);
+					else 
+					CLEAR_BIT(GPIO_PORTD_DATA_R,pinnum);
+					break;
+					
+				case PORTE:
+					if ( type == HIGH )
+					SET_BIT(GPIO_PORTE_DATA_R,pinnum);
+					else 
+					CLEAR_BIT(GPIO_PORTE_DATA_R,pinnum);
+					break;	
+					
+				case PORTF:
+					if ( type == HIGH )
+					SET_BIT(GPIO_PORTF_DATA_R,pinnum);
+					else 
+					CLEAR_BIT(GPIO_PORTF_DATA_R,pinnum);
+					break;
+				default:
+						break;
+			}
+}
+/*
+	Function Name        : Dio_Read_Pin
+	Function Returns     : unsigned char
+	Function Arguments   : PortName portname,PinNum pinnum
+	Function Description : Returns 1 if the value of the given pin is high and zero if the value is low
+*/
+short Dio_Read_Pin(PortName portname,PinNum pinnum)
+	{
+			switch(portname)
+			{
+				case PORTA:
+					return IS_BIT_SET(GPIO_PORTA_DATA_R,pinnum);
+				case PORTB:
+					return IS_BIT_SET(GPIO_PORTB_DATA_R,pinnum);
+				case PORTC:
+					return IS_BIT_SET(GPIO_PORTC_DATA_R,pinnum);
+				case PORTD:
+					return IS_BIT_SET(GPIO_PORTD_DATA_R,pinnum);
+				case PORTE:
+					return IS_BIT_SET(GPIO_PORTE_DATA_R,pinnum);
+				case PORTF:
+					return IS_BIT_SET(GPIO_PORTF_DATA_R,pinnum);
+				default:
+					return 0;
+			}
+	}
+	/*
+	Function Name        : Dio_Toggle_Pin
+	Function Returns     : void
+	Function Arguments   : PortName portname,PinNum pinnum
+	Function Description : Reverse the value of the given pin in the given port.
+*/
+	void Dio_Toggle_Pin(PortName portname,PinNum pinnum)
+{
+	switch(portname)
+			{
+				case PORTA:
+					 TOGGLE(GPIO_PORTA_DATA_R,pinnum);
+				case PORTB:
+					TOGGLE(GPIO_PORTB_DATA_R,pinnum);
+				case PORTC:
+					TOGGLE(GPIO_PORTC_DATA_R,pinnum);
+				case PORTD:
+					TOGGLE(GPIO_PORTD_DATA_R,pinnum);
+				case PORTE:
+					TOGGLE(GPIO_PORTE_DATA_R,pinnum);
+				case PORTF:
+					TOGGLE(GPIO_PORTF_DATA_R,pinnum);
+				default:
+					return;
+			}
+}
---
+++ DIO.h
@@ -0,0 +1,39 @@
+/*
+//  This file have all HAL layes and API's to be familer with 
+//	Tiva C ports and pins configrations
+//  Author: Mostafa Ashraf
+//  Date: 26/02/2020
+*/
+#ifndef DIO_H_
+#define DIO_H_
+#include "StabdardTypes.h"
+#include "tm4c123gh6pm.h"
+/* macro like function to be familer 
+	 with ports Pins in 
+	 (HardWare abstraction Layer "HAL")layer
+*/
+#define SET_BIT(REG,BIT) REG|=(1<<BIT) 
+#define	CLEAR_BIT(REG,BIT) REG = REG & ~(1<<BIT)
+#define TOGGLE(REG,BIT)	(REG ^= (1<<BIT))
+#define IS_BIT_SET(REG,BIT) ((REG &(1<<(BIT)))>>(BIT))
+#define IS_BIT_CLREAR(REG,BIT) !(IS_BIT_SET(REG,BIT))
+
+// Magic number for unlock certin port
+#define MAGIC_NUMBER 0x4C4F434B
+
+typedef enum {PORTA,PORTB,PORTC,PORTD,PORTE,PORTF,}PortName;
+
+typedef enum {PIN0,PIN1,PIN2,PIN3,PIN4,PIN5,PIN6,PIN7}PinNum;
+
+typedef enum {LOW,HIGH}Signal;
+	
+typedef enum {INPUT,OUTPUT,INTERNAL_PULL_UP,INTERNAL_PULL_DOWN}Mode;
+
+
+//#define GPIO_PORTA_AHB_DIR_R    (*((volatile unsigned long *)0x40058400))
+//#define GPIO_PORTA_AHB_DATA_R   (*((volatile unsigned long *)0x400583FC))
+void Dio_config(PortName portname,PinNum pinnumber,Mode mode);
+void Dio_Write_Pin(PortName portname,PinNum pinnum,Signal type);
+short Dio_Read_Pin(PortName portname,PinNum pinnum);
+void Dio_Toggle_Pin(PortName portname,PinNum pinnum);
+#endif
