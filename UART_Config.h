5625634ecc8d21f41f1a16f9363be8e7f93f73e6
Mostafa Ashraf <mostaphaashraf1996@gmail.com>
2020-03-13 15:50:45 +0200

    UART configuration header file

---
+++ UART_Config.h
@@ -0,0 +1,24 @@
+#ifndef UART_CONFIG_H_
+#define UART_CONFIG_H_
+#include	"tm4c123gh6pm.h"
+#include	"StabdardTypes.h"
+
+#define SYSTEM_CLOCK	16000000
+#define DESIRED_BAUD_RATE	9600
+
+#define BRC_DIVSIOR		(float_32)(SYSTEM_CLOCK/(float_32)(16*(float_32)DESIRED_BAUD_RATE))
+
+#define INTEGER_VALUE		((int_32)BRC_DIVSIOR)
+
+#define FRACTIONAL_VALUE  (int_32)((((float_32)BRC_DIVSIOR-(float_32)INTEGER_VALUE)*(float_32)64)+(float_32).5)
+
+
+
+
+#define UARTn_FLAG_REG	 	UART0_FR_R 
+#define UARTn_DATA_REG		UART0_DR_R
+#define UARTn_INTBD_REG		UART0_IBRD_R
+#define UARTn_FRACBR_REG	UART0_FBRD_R 
+
+
+#endif
